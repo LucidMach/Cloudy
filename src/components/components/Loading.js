99a7f26934042081ca17b3d534bcc1b74de107ea
LucidMach <nukalasuraj7@gmail.com>
2021-05-09 00:39:02 +0530

    404 dealings

--- src/components/components/Loading.js
+++ src/components/components/Loading.js
@@ -3,6 +3,12 @@ import "./Loading.css";
 const Loading = () => {
   return (
     <div className="App">
+      <p style={{ color: "#9b78f2", margin: "1rem" }}>
+        <strong>
+          NOTE : if the app takes too long to respond, you probabily have a typo
+          in the city name.
+        </strong>
+      </p>
       <div className="Loading"></div>
       <button className="btn" onClick={() => window.location.reload()}>
         CANCEL
